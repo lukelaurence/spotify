ae31f8bd13a0cd7104a66b1aed825e4a9737be52
lukelaurence <48737821+lukelaurence@users.noreply.github.com>
2022-09-03 23:56:39 -0700

    Take list of songs and output Spotify links

---
+++ gettracks.py
@@ -0,0 +1,37 @@
+import os, sys
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotifycredentials import set_credentials
+
+set_credentials()
+
+spotipy_client_id = os.environ.get('SPOTIPY_CLIENT_ID')
+spotipy_client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
+spotipy_redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI')
+
+scope = "user-read-private"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+def constructquery(album,track,artist,year,upc,hipster,new,isrc,genre):
+	for key,value in locals().items():
+		if value:
+			yield f"{key}:{value}"
+
+def makequery(album=None,track=None,artist=None,year=None,upc=None,hipster=None,new=None,isrc=None,genre=None):
+	return ' '.join([x for x in constructquery(*locals().values())])
+
+def geturl(album=None,track=None,artist=None,year=None,upc=None,hipster=None,new=None,isrc=None,genre=None):
+	query_string = makequery(*locals().values())
+	results = sp.search(q=query_string,limit=1,type='track')
+	return results['tracks']['items'][0]['external_urls']['spotify']
+
+with open('songs.txt','r') as f:
+	with open('songurls.txt','x') as f2:
+		sys.stdout = f2
+		for x in f:
+			p = x.find('(')
+			track = x[:p-1]
+			year = x[p+1:p+5]
+			artist = x[p+8:].title()
+			print(geturl(track=track,artist=artist))
